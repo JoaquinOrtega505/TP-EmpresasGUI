7a44df5a0f8671459fec3d66cc89ec1b4142bc32
JoaquinOrtega505 <72822153+JoaquinOrtega505@users.noreply.github.com>
2023-09-09 17:57:34 -0300

    .

--- src/tp/empresasgui/VistaEmpresas.java
+++ src/tp/empresasgui/VistaEmpresas.java
@@ -104,16 +104,16 @@ public class VistaEmpresas extends javax.swing.JFrame{
 
         jcCategoria.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Gerente", "Jefe", "Administrativo" }));
         jcCategoria.setSelectedIndex(-1);
-        jcCategoria.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jcCategoriaActionPerformed(evt);
-            }
-        });
 
         jBMostrarEmpleado.setText("Mostrar Empleados");
         jBMostrarEmpleado.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 jBMostrarEmpleadoMouseClicked(evt);
+            }
+        });
+        jBMostrarEmpleado.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBMostrarEmpleadoActionPerformed(evt);
             }
         });
 
@@ -367,89 +367,36 @@ public class VistaEmpresas extends javax.swing.JFrame{
     }//GEN-LAST:event_jbCrearEmpresaActionPerformed
 
     private void jBGuardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBGuardarActionPerformed
-          
+
         try {
-        String dni = jtDocumento.getText();
-        String nombre = jtNombre.getText();
-        String apellido = jtApellido.getText();
-        String categoria = (String) jcCategoria.getSelectedItem();
-        String empresa = (String) jcEmpresa.getSelectedItem();
-        String sueldo = (String) jtSueldo.getText();
+            String dni = jtDocumento.getText();
+            String nombre = jtNombre.getText();
+            String apellido = jtApellido.getText();
+            String categoria = (String) jcCategoria.getSelectedItem();
+            String empresa = (String) jcEmpresa.getSelectedItem();
+            String sueldo = (String) jtSueldo.getText();
 
-        Empleado empleado = new Empleado(dni, nombre, apellido, categoria, empresa, sueldo, listaDeEmpleados);
-        
-        listaDeEmpleados.add(empleado);
-        
+            if (dni.isEmpty() || nombre.isEmpty() || apellido.isEmpty() || categoria == null || empresa == null || sueldo.isEmpty()) {
+                JOptionPane.showMessageDialog(this, "Por favor, complete todos los campos antes de guardar.", "Error", JOptionPane.ERROR_MESSAGE);
+            } else {
+                Empleado empleado = new Empleado(dni, nombre, apellido, categoria, empresa, sueldo, listaDeEmpleados);
+                listaDeEmpleados.add(empleado);
+                JOptionPane.showMessageDialog(this, "Empleado guardado");
 
-        
-        System.out.println("Empleado guardado:");
-        System.out.println("DNI: " + dni);
-        System.out.println("Nombre: " + nombre);
-        System.out.println("Apellido: " + apellido);
-        System.out.println("Categoría: " + categoria);
-        System.out.println("Empresa: " + empresa);
-        System.out.println("Sueldo: " + sueldo);
+                jtDocumento.setText("");
+                jtNombre.setText("");
+                jtApellido.setText("");
+                jcCategoria.setSelectedItem(null); // Limpia el combo box
+                jcEmpresa.setSelectedItem(null); // Limpia el combo box
+                jtSueldo.setText("");
+            }
 
-        JOptionPane.showMessageDialog(this, "Empleado guardado");
+        } catch (NumberFormatException nf) {
+            JOptionPane.showMessageDialog(this, "Ingrese el Sueldo", "Error", JOptionPane.INFORMATION_MESSAGE);
 
-        jtDocumento.setText("");
-        jtNombre.setText("");
-        jtApellido.setText("");
-        jcCategoria.setSelectedItem(null); // Limpia el combo box
-        jcEmpresa.setSelectedItem(null); // Limpia el combo box
-        jtSueldo.setText("");
-    } catch (NumberFormatException nf) {
-        JOptionPane.showMessageDialog(this, "Ingrese el Sueldo", "Error", JOptionPane.INFORMATION_MESSAGE);
-    }
-
-
-
-
-
-
-
-
-//        try{
-//                String dni = jtDocumento.getText();
-//                String nombre = jtNombre.getText();
-//                String apellido = jtApellido.getText();
-//                String categoria = (String) jcCategoria.getSelectedItem();
-//                String empresa = (String) jcEmpresa.getSelectedItem();
-//                String sueldo = (String) jtSueldo.getText();
-//
-//                Empleado nvoEmpleado = new Empleado(dni, nombre, apellido, categoria, empresa, sueldo, listaDeEmpleados);
-//
-//            
-//            JOptionPane.showMessageDialog(this, "empleado guardado");
-//           
-//            jtDocumento.setText("");
-//            jtNombre.setText("");
-//            jtApellido.setText("");
-//            jcCategoria.setSelectedItem(null); // Limpia el combo box
-//            jcEmpresa.setSelectedItem(null); // Limpia el combo box
-//            jtSueldo.setText("");
-//        } catch (NumberFormatException nf){
-//        JOptionPane.showMessageDialog(this, "ingrese numeros en el campo sueldo");
-//        }
-//        
-//        
-        
-//       String empresaSeleccionada = (String) jcEmpresa.getSelectedItem();
-//       String empresa = (String) jcEmpresa.getSelectedItem();
-//
-//        String dni = jtDocumento.getText();
-//        String nombre = jtNombre.getText();
-//        String apellido = jtApellido.getText();
-//        String categoria = (String) jcCategoria.getSelectedItem();
-//       // String empresa = (String) jcEmpresa.getSelectedItem();
-//        double sueldo = Double.parseDouble(jtSueldo.getText());
-        
-
-
-        
-       //Empleado empleado = new Empleado(dni, nombre, apellido, categoria, sueldo,empresa);
+        }
     
-       
+      
         
     
     }//GEN-LAST:event_jBGuardarActionPerformed
@@ -457,124 +404,75 @@ public class VistaEmpresas extends javax.swing.JFrame{
     private void jBMostrarEmpleadoMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jBMostrarEmpleadoMouseClicked
       
     // Obtener la empresa seleccionada
-    String empresaSeleccionada = (String) jcEmpresa.getSelectedItem();
-
-    // Buscar la empresa seleccionada en la lista de empresas
-    Empresa empresa = buscarEmpresa(empresaSeleccionada);
-
-    if (empresa != null) {
-        // Obtener la lista de empleados asociados a la empresa
-        ArrayList<Empleado> empleadosEmpresa = empresa.getEmpleados();
-        
-
-        if (!empleadosEmpresa.isEmpty()) {
-            // Mostrar un cuadro emergente para seleccionar un empleado de la empresa
-            String[] empleadosNombres = new String[empleadosEmpresa.size()];
-            for (int i = 0; i < empleadosEmpresa.size(); i++) {
-                empleadosNombres[i] = empleadosEmpresa.get(i).getNombre() + " " + empleadosEmpresa.get(i).getApellido();
-            }
-
-            String empleadoSeleccionado = (String) JOptionPane.showInputDialog(this, "Selecciona un empleado:", "Empleados de " + empresa.getRazonSocial(), JOptionPane.PLAIN_MESSAGE, null, empleadosNombres, empleadosNombres[0]);
-
-            // Buscar el empleado seleccionado
-            Empleado empleado = null;
-            for (Empleado emp : empleadosEmpresa) {
-                if ((emp.getNombre() + " " + emp.getApellido()).equals(empleadoSeleccionado)) {
-                    empleado = emp;
-                    break;
-                }
-            }
-
-            // Mostrar los datos del empleado seleccionado
-            if (empleado != null) {
-                String mensaje = "Nombre: " + empleado.getNombre() + "\n" +
-                                 "Apellido: " + empleado.getApellido() + "\n" +
-                                 "Categoría: " + empleado.getCategoria() + "\n" +
-                                 "Sueldo: " + empleado.getSueldo() + "\n" +
-                                 "Empresa: " + empleado.getEmpresa();
-
-                JOptionPane.showMessageDialog(this, mensaje, "Datos del Empleado", JOptionPane.INFORMATION_MESSAGE);
-            }
-        } else {
-            JOptionPane.showMessageDialog(this, "No hay empleados registrados para esta empresa.", "Error", JOptionPane.ERROR_MESSAGE);
-        }
-    } else {
-        JOptionPane.showMessageDialog(this, "La empresa seleccionada no existe.", "Error", JOptionPane.ERROR_MESSAGE);
-    }
-
-
-                                                 
-//    // Obtener los valores de los campos de entrada
-//    String nombre = jtNombre.getText();
-//    String apellido = jtApellido.getText();
-//    String categoria = (String) jcCategoria.getSelectedItem();
-//    String sueldoStr = jtSueldo.getText();
 //    String empresaSeleccionada = (String) jcEmpresa.getSelectedItem();
-//    
-//    try {
-//        // Convertir el sueldo a double
-//        double sueldo = Double.parseDouble(sueldoStr);
-//        
-//        // Buscar la empresa seleccionada en la lista de empresas
-//        Empresa empresa = buscarEmpresa(empresaSeleccionada);
-//        
-//        // Crear un nuevo empleado con los datos ingresados
-//        Empleado empleado = new Empleado(nombre, apellido, categoria, sueldo, empresa);
-//        
-//        // Mostrar un cuadro emergente con los datos del empleado
-//        String mensaje = "Nombre: " + empleado.getNombre() + "\n" +
-//                         "Apellido: " + empleado.getApellido() + "\n" +
-//                         "Categoría: " + empleado.getCategoria() + "\n" +
-//                         "Sueldo: " + empleado.getSueldo() + "\n" +
-//                         "Empresa: " + empleado.getEmpresa();
-//        
-//        JOptionPane.showMessageDialog(this, mensaje, "Datos del Empleado", JOptionPane.INFORMATION_MESSAGE);
-//        
-////        // Limpiar los campos de entrada
-////        jtNombre.setText("");
-////        jtApellido.setText("");
-////        jtSueldo.setText("");
-//    } catch (NumberFormatException e) {
-//        // Manejar una excepción si el sueldo no es un número válido
-//        JOptionPane.showMessageDialog(this, "El sueldo debe ser un número válido.", "Error", JOptionPane.ERROR_MESSAGE);
-//    } catch (NullPointerException e) {
-//        // Manejar una excepción si la empresa seleccionada no se encuentra en la lista de empresas
+//
+//    // Buscar la empresa seleccionada en la lista de empresas
+//    Empresa empresa = buscarEmpresa(empresaSeleccionada);
+//  
+//    if (empresa != null) {
+        
+        // Obtener la lista de empleados asociados a la empresa
+//        ArrayList<Empleado> empleadosEmpresa = empresa.getEmpleados();
+//
+//        if (empleadosEmpresa != null&& empleadosEmpresa.isEmpty()) {
+//              JOptionPane.showMessageDialog(null, "2");
+//            // Mostrar un cuadro emergente para seleccionar un empleado de la empresa
+//            String[] empleadosNombres = new String[empleadosEmpresa.size()];
+//            for (int i = 0; i < empleadosEmpresa.size(); i++) {
+//                  JOptionPane.showMessageDialog(null, "3");
+//                empleadosNombres[i] = empleadosEmpresa.get(i).getNombre() + " " + empleadosEmpresa.get(i).getApellido();
+//            }
+//            System.out.println("que onda?");  JOptionPane.showMessageDialog(null, "4");
+//
+//            String empleadoSeleccionado = (String) JOptionPane.showInputDialog(this, "Selecciona un empleado:", "Empleados de " + empresa.getRazonSocial(), JOptionPane.PLAIN_MESSAGE, null, empleadosNombres, empleadosNombres[0]);
+//
+//            // Buscar el empleado seleccionado
+//            Empleado empleado = null;
+//            for (Empleado emp : empleadosEmpresa) {
+//                if ((emp.getNombre() + " " + emp.getApellido()).equals(empleadoSeleccionado)) {
+//                    empleado = emp;
+//                      JOptionPane.showMessageDialog(null, "5");
+//                    break;
+//                }
+//            }
+//
+//            // Mostrar los datos del empleado seleccionado
+//            if (empleado != null) {
+//                String mensaje = "Nombre: " + empleado.getNombre() + "\n" +
+//                                 "Apellido: " + empleado.getApellido() + "\n" +
+//                                 "Categoría: " + empleado.getCategoria() + "\n" +
+//                                 "Sueldo: " + empleado.getSueldo() + "\n" +
+//                                 "Empresa: " + empleado.getEmpresa();
+//
+//                JOptionPane.showMessageDialog(this, mensaje, "Datos del Empleado", JOptionPane.INFORMATION_MESSAGE);
+//            }
+//        }
+//        else {
+//            JOptionPane.showMessageDialog(this, "No hay empleados registrados para esta empresa.", "Error", JOptionPane.ERROR_MESSAGE);
+//        }
+//    } else {
 //        JOptionPane.showMessageDialog(this, "La empresa seleccionada no existe.", "Error", JOptionPane.ERROR_MESSAGE);
 //    }
-//
 
 
-//String empresaCbox=(String) jcEmpresa.getSelectedItem();
-//        if(empresaCbox != null && !empresaCbox.isEmpty()){
-//                String dato = "";
-//                for (Empleado empleado: listaDeEmpleados){
-//                    if (empleado.getEmpresa().equals(empresaCbox)) {
-//                        dato += "DNI: " + empleado.getDni() + "\n";
-//                        dato += "Nombre y Apellido: " + empleado.getNombre() + " " + empleado.getApellido() + "\n";
-//                        dato += "Categoría: " + empleado.getCategoria() + "\n";                        
-//                        dato += "Sueldo: " +empleado.getSueldo();
-//           }
-//                    JOptionPane.showMessageDialog(this, dato, "Empleado de: " + empresaCbox, JOptionPane.INFORMATION_MESSAGE);
-//        }
-////                if(!dato.isEmpty()){
-////                    JOptionPane.showMessageDialog(this, dato, "empleado de: " + empresaCbox, JOptionPane.INFORMATION_MESSAGE);
-////                }else{
-////                    JOptionPane.showMessageDialog(datos, "Esta empresa no tiene empleados");
-////                }
-////        }else{
-////            JOptionPane.showMessageDialog(datos, "Seleccione Una Empresa");
-//        }
-//
-//        
-//              
+           
             
          
         
     }//GEN-LAST:event_jBMostrarEmpleadoMouseClicked
-
-    private void jcCategoriaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jcCategoriaActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jcCategoriaActionPerformed
+    
+    private void jBMostrarEmpleadoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBMostrarEmpleadoActionPerformed
+        Empresa empresaSeleccionada = (Empresa) jcEmpresa.getSelectedItem();
+                if (empresaSeleccionada != null) {
+                    // Mostrar los empleados de la empresa seleccionada
+                    JOptionPane.showMessageDialog(this,"Empleados de " + empresaSeleccionada.getRazonSocial() + ":\n" + empresaSeleccionada.getEmpleados());
+                } else {
+                    JOptionPane.showMessageDialog(this, "Seleccione una empresa.");
+                }
+            
+       
+        
+    }//GEN-LAST:event_jBMostrarEmpleadoActionPerformed
 
     
     /**
